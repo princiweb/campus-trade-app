a53947d4688620ac6cb163672f1a90d0f13f88e1
Victor Miguez <victorcineis@gmail.com>
2016-01-28 03:48:11 -0200

    front end do app

--- campus-trade-app.js
+++ campus-trade-app.js
@@ -1,23 +1,24 @@
+Trades = new Mongo.Collection('trades');
+
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  moment.locale('pt-br');
+
+  Template.body.helpers({
+    trades: function() {
+      return Trades.find({}, {sort: {createdAt: -1} }).fetch();
+    },
+
+    timeFromNow: function(date) {
+      return moment(date).fromNow();
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+  
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    process.env.MONGO_URL =  'mongodb://trade:trade@ds051655.mongolab.com:51655/campus-trade';
   });
 }
