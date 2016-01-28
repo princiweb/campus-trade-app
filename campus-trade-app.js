e83602cea5c97b4668d6b5d09744537215ed806f
Gabriel Ribeiro <biel.ribeiro1990@gmail.com>
2016-01-28 02:08:22 -0200

    first commit

---
+++ campus-trade-app.js
@@ -0,0 +1,23 @@
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+  });
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    process.env.MONGO_URL =  'mongodb://trade:trade@ds051655.mongolab.com:51655/campus-trade';
+  });
+}
