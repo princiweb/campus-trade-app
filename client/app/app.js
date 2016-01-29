99443a4ce26adfdc5c30ddec07f6e1ea52c7855d
Victor Perin <me@victorperin.com>
2016-01-29 19:39:44 -0200

    feat(pesquisa): inserindo pesquisa

--- client/app/app.js
+++ client/app/app.js
@@ -4,10 +4,26 @@ Meteor.subscribe('trades');
 
 Template.body.helpers({
   trades: function() {
-    return Trades.find({}, {sort: {createdAt: -1} }).fetch();
+    var options = {};
+    if(Session.get('busca-offered')){
+      options.offered = Session.get('busca-offered');
+    }
+    if(Session.get('busca-wanted')){
+      options.wanted = Session.get('busca-wanted');
+    }
+    return Trades.find(options, {sort: {createdAt: -1} }).fetch();
   },
 
   timeFromNow: function(date) {
     return moment(date).fromNow();
+  }
+});
+
+Template.body.events({
+  'keyup #busca-offered': function(event, template){
+    Session.set('busca-offered', event.target.value);
+  },
+  'keyup #busca-wanted': function(){
+    Session.set('busca-wanted', event.target.value);
   }
 });
--- server/publish.js
+++ server/publish.js
@@ -1,3 +1,3 @@
 Meteor.publish('trades', function () {
   return Trades.find();
-})
+});
